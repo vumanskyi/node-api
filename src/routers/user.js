21f3f8692d234a6d3d8c439fba279e3fdb4448ab
Vlad Umanskyi <vladumanskyi@gmail.com>
2019-10-21 16:52:40 +0300

    Update user.js
    Bug fix

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/User');
-
+const auth = requeire('../middleware/auth');
 const router = express.Router();
 
 router.post('/users', async (req, res) => {
